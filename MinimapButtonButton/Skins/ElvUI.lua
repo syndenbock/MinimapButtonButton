5de89ed1c68f0363d8172289304cb9df01d71396
syndenbock <syndenbock>
2021-05-08 16:21:38 +0200

    Wait with button skinning until PLAYER_LOGIN was fired
    This is to prevent unnecessary skinning calls beforehand.

--- MinimapButtonButton/Skins/ElvUI.lua
+++ MinimapButtonButton/Skins/ElvUI.lua
@@ -2,33 +2,36 @@ if (not _G.IsAddOnLoaded('ElvUI')) then return end
 
 local _, addon = ...;
 
-local min = _G.min;
+addon.registerEvent('PLAYER_LOGIN', function ()
+  local ENGINE = _G.ElvUI[1];
 
-local ENGINE = _G.ElvUI[1];
+  local function applySkin ()
+    local media = ENGINE.media;
+    local backdrop = {
+      bgFile = media.glossTex,
+      edgeFile = media.blankTex,
+      edgeSize = 1,
+      insets = {
+        left = 1,
+        right = 1,
+        top = 1,
+        bottom = 1,
+      },
+    };
 
-local function applySkin ()
-  local media = ENGINE.media;
-  local backdrop = {
-    bgFile = media.glossTex,
-    edgeFile = media.blankTex,
-    edgeSize = 1,
-    insets = {
-      left = 1,
-      right = 1,
-      top = 1,
-      bottom = 1,
-    },
-  };
+    local function skinFrame (frame)
+      frame:SetBackdrop(backdrop);
+      frame:SetBackdropColor(unpack(media.backdropcolor));
+      frame:SetBackdropBorderColor(unpack(media.bordercolor));
+    end
 
-  local function skinFrame (frame)
-    frame:SetBackdrop(backdrop);
-    frame:SetBackdropColor(unpack(media.backdropcolor));
-    frame:SetBackdropBorderColor(unpack(media.bordercolor));
+    skinFrame(addon.shared.buttonContainer);
+    skinFrame(addon.shared.mainButton);
+    addon.shared.logo:SetVertexColor(unpack(media.rgbvaluecolor));
   end
 
-  skinFrame(addon.shared.buttonContainer);
-  skinFrame(addon.shared.mainButton);
-  addon.shared.logo:SetVertexColor(unpack(media.rgbvaluecolor));
-end
+  applySkin();
+  ENGINE.valueColorUpdateFuncs[applySkin] = true;
 
-ENGINE.valueColorUpdateFuncs[applySkin] = true;
+  return true;
+end);
