48edb26e1c61ac5f15077ca0313e0b9c9cbc7067
syndenbock <syndenbock>
2021-05-21 19:08:13 +0200

    Colorize addon name in addon messages

--- MinimapButtonButton/Utils.lua
+++ MinimapButtonButton/Utils.lua
@@ -1,7 +1,9 @@
 local addonName, addon = ...;
 
+local ADDON_MESSAGE_PREFIX = '|cff00ffff' .. addonName .. '|r' .. ': ';
+
 local function printAddonMessage (message)
-  print(addonName .. ': ' .. message);
+  print(ADDON_MESSAGE_PREFIX .. message);
 end
 
 local function printReloadMessage (message)
