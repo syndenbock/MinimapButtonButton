13882afafb486be5f0179db64dd85339fc209ee5
syndenbock <syndenbock>
2022-01-02 02:16:31 +0100

    Implement Tukui skin

---
+++ MinimapButtonButton/Skins/Tukui.lua
@@ -0,0 +1,33 @@
+if (not _G.IsAddOnLoaded('Tukui')) then return end
+
+local _, addon = ...;
+
+local function skinFrame (frame, config)
+  local media = config.Medias;
+  local edgeSize = 3;
+
+  local backdrop = {
+    bgFile = media.Blank,
+    edgeFile = media.Glow,
+    edgeSize = edgeSize,
+    insets = {
+      left = edgeSize,
+      right = edgeSize,
+      top = edgeSize,
+      bottom = edgeSize,
+    }
+  };
+
+  frame:SetBackdrop(backdrop);
+  frame:SetBackdropColor(unpack(media.BackdropColor));
+  frame:SetBackdropBorderColor(unpack(media.BorderColor));
+end
+
+addon.registerEvent('PLAYER_LOGIN', function ()
+  local CONFIG = _G.Tukui[2];
+
+  skinFrame(addon.shared.buttonContainer, CONFIG);
+  skinFrame(addon.shared.mainButton, CONFIG);
+  addon.shared.logo:SetVertexColor(unpack(CONFIG.Medias.BorderColor));
+  return true;
+end);
