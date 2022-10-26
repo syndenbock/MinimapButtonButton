syndenbock/MinimapButtonButton

commit 923324268ed05bffeceb62ebd8ab8d6e81e28c57

Improve minimap button name patterns to be more specific

MinimapButtonButton/Logic/Main.lua
@@ -71,11 +71,11 @@ local function isMinimapButton (frame)
   end
 
   local patterns = {
-    'LibDBIcon10_',
-    'MinimapButton',
-    'MinimapFrame',
-    'MinimapIcon',
-    '[-_]Minimap',
+    '^LibDBIcon10_',
+    'MinimapButton$',
+    'MinimapFrame$',
+    'MinimapIcon$',
+    '[-_]Minimap$',
   };
 
   for _, pattern in ipairs(patterns) do

commit 0c9964a946d0130b9fbd8b83f2573663898d1285

Update minimap button name patterns to be less specific
Changes in commit 923324268ed05bffeceb62ebd8ab8d6e81e28c57 caused some buttons
to no longer be collected.

MinimapButtonButton/Logic/Main.lua
@@ -72,10 +72,11 @@ local function isMinimapButton (frame)
 
   local patterns = {
     '^LibDBIcon10_',
-    'MinimapButton$',
-    'MinimapFrame$',
-    'MinimapIcon$',
-    '[-_]Minimap$',
+    'MinimapButton',
+    'MinimapFrame',
+    'MinimapIcon',
+    '[-_]Minimap[-_]',
+    'Minimap$',
   };
 
   for _, pattern in ipairs(patterns) do