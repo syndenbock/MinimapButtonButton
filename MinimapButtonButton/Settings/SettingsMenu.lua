9a5ea33caf21f2e8a76648836a5aff0c8bb53e74
syndenbock <syndenbock>
2026-01-23 22:08:53 +0100

    feat: Implement hidecompartment setting in settings panel

--- MinimapButtonButton/Settings/SettingsMenu.lua
+++ MinimapButtonButton/Settings/SettingsMenu.lua
@@ -7,6 +7,7 @@ local category = Settings.RegisterVerticalLayoutCategory(addonName);
 
 local Main = addon.import('Logic/Main');
 local Options = addon.import('Logic/Options');
+local Enhancements = addon.import('Features/Enhancements');
 local Layout = addon.import('Layouts/Main');
 
 local addonOptions = Options.getAll();
@@ -17,11 +18,11 @@ local function registerDropdown (key, defaultValue, optionsGetter, options)
   local setValue = options.setValue;
   local setting = Settings.RegisterAddOnSetting(category, variable, key, addonOptions, type(defaultValue), name, defaultValue);
 
-  setting:SetValueChangedCallback(function (self, value)
-    if (setValue ~= nil) then
+  if (setValue ~= nil) then
+    setting:SetValueChangedCallback(function (_, value)
       setValue(value);
-    end
-  end);
+    end);
+  end
 
   Settings.CreateDropdown(category, setting, optionsGetter, options.tooltip);
 end
@@ -34,14 +35,29 @@ local function registerSlider(key, defaultValue, options)
   local setValue = options.setValue;
   local setting = Settings.RegisterAddOnSetting(category, variable, key, addonOptions, type(defaultValue), name, defaultValue);
 
-  setting:SetValueChangedCallback(function (_, value)
-    if (setValue ~= nil) then
+  if (setValue ~= nil) then
+    setting:SetValueChangedCallback(function (_, value)
       setValue(value);
-    end
-  end);
+    end);
+  end
 
-  sliderOptions:SetLabelFormatter(options.label or MinimalSliderWithSteppersMixin.Label.Left);
+  sliderOptions:SetLabelFormatter(options.label or MinimalSliderWithSteppersMixin.Label.Right);
   Settings.CreateSlider(category, setting, sliderOptions, options.tooltip)
+end
+
+local function registerCheckbox (key, defaultValue, options)
+  local name = options.name or key;
+  local variable = addonName .. '_' .. key;
+  local setValue = options.setValue;
+  local setting = Settings.RegisterAddOnSetting(category, variable, key, addonOptions, type(defaultValue), name, defaultValue);
+
+  if (setValue ~= nil) then
+    setting:SetValueChangedCallback(function (_, value)
+      setValue(value);
+    end);
+  end
+
+  Settings.CreateCheckbox(category, setting, options.tooltip);
 end
 
 local function registerSettingsMenu()
@@ -98,6 +114,19 @@ local function registerSettingsMenu()
       end
     end
   });
+
+  if (Enhancements.compartment ~= nil) then
+    registerCheckbox("hidecompartment", false, {
+      name = "Hide addon compartment frame",
+      setValue = function (value)
+        if (value == true) then
+          Enhancements.hideCompartmentFrame();
+        else
+          Enhancements.showCompartmentFrame();
+        end
+      end
+    });
+  end
 
   Settings.RegisterAddOnCategory(category);
 end
